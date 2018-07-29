b01390fe61e4f92f5c683cff0bf889a44e6e5f36
Nishchay Kaushik <kaushiknishchay@gmail.com>
2018-07-29 17:20:23 +0530

    Add material ui theme

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,24 +1,75 @@
 import React, { Component } from 'react';
-import '../styles/css/App.css';
-import Strings from '../constants/strings';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar/Toolbar';
+import IconButton from '@material-ui/core/IconButton/IconButton';
+import Typography from '@material-ui/core/Typography/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
 
 class App extends Component {
   render() {
+    const auth = true;
     return (
       <div>
-        <header className="header-base">
-          <h1 className="header-title">
-            {Strings.appName}
-          </h1>
-          <div className="header-iconwrap">
-            <a className="header-icon-item" href={'#/'}>
-              <i className="material-icons header-icon">
-                account_circle
-              </i>
-            </a>
-          </div>
-        </header>
+        <AppBar
+          position="static"
+          color="primary"
+        >
+          <Toolbar>
+            <IconButton
+              style={{
+                marginLeft: -12,
+                marginRight: 20,
+              }}
+              color="inherit"
+              aria-label="Menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="title"
+              color="inherit"
+              style={{
+                flexGrow: 1,
+              }}
+            >
+              Photos
+            </Typography>
+            {auth && (
+              <div>
+                <IconButton
+                  aria-haspopup="true"
+                  onClick={this.handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleClose}>
+                    Profile
+                  </MenuItem>
+                  <MenuItem onClick={this.handleClose}>
+                    My account
+                  </MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
         <div className="content">
           <p className="App-intro">
             To get started, edit
